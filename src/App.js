bc3fb9c2762bc793434e14dd4e5af1d845a5380b
Adrian-Chambers <47733972+Adrian-Chambers@users.noreply.github.com>
2021-01-23 16:04:28 -0500

    cleaned up code and added scrollbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import CardList from './CardList'
 import SearchBox from './SearchBox';
-import { robots } from './robots';
-import { render } from '@testing-library/react';
+import Scroll from './Scroll';
 import './App.css'
 
 
@@ -10,9 +9,15 @@ class App extends React.Component {
     constructor(){
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ' '
         }
+    }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({robots: users}));
     }
 
     onSearchChange = (event) => {
@@ -23,13 +28,19 @@ class App extends React.Component {
         const filteredRobots = this.state.robots.filter(robots => {
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         });
-        return( 
-            <div className="tc">
-                <h1 className="f2">RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange} />
-                <CardList robots={filteredRobots}/> 
-            </div>
-        );
+        if(this.state.robots.length === 0){
+            return <h1 className="tc">Loading</h1>
+        } else{
+            return( 
+                <div className="tc">
+                    <h1 className="f2">RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <Scroll>
+                        <CardList robots={filteredRobots}/> 
+                    </Scroll>
+                </div>
+            ); 
+        }
     }
 }
 
